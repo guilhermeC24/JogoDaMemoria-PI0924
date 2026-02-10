27e9c0b815347caee9d841946b26f44a7c31d252
Guilherme Cabrita <gui.celorindo@gmail.com>
2026-02-10 09:59:18 +0000

    2o commit

--- jogo.py
+++ jogo.py
@@ -12,3 +12,16 @@ class Quadrado:
         self.cor = cor
         self.retangulo = canvas.create_rectangle(x, y, x+tamanho, y+tamanho, fill="white", outline="black")
         self.ativo = False
+        
+    def pressionar(self):
+        self.canvas.itemconfig(self.retangulo, fill=self.cor)
+        self.ativo = True
+
+    def despressionar(self):
+        self.canvas.itemconfig(self.retangulo, fill="white")
+        self.ativo = False
+
+    def foi_clicado(self, evento):
+        x, y = evento.x, evento.y
+        coords = self.canvas.coords(self.retangulo)
+        return coords[0] <= x <= coords[2] and coords[1] <= y <= coords[3]
